6cb6034ea29e6f306ea9bcad7ca260a823e7c5eb
fjuribe <fjuribeui@gmail.com>
2019-10-29 17:29:34 -0300

    ion-list Listas en ionic -Parte1

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  componentes:Componente[]=[
+   {
+   	icon:'american-football',
+   	name:'Action sheet',
+   	redirecTo:'/action-sheet'
+   },
+    {
+   	icon:'appstore',
+   	name:'Alert',
+   	redirecTo:'/alert'
+   }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+
+interface Componente{
+	icon:string;
+	name:string;
+	redirecTo:string;
 }
