b975d4140ca03946eaf7bf401c33067b106d328d
fjuribe <fjuribeui@gmail.com>
2019-10-31 17:20:53 -0300

    Agregar componentes: ion-button

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -23,6 +23,12 @@ export class InicioPage implements OnInit {
      name:'avatar',
      redirecTo:'/avatar'
    }
+   ,
+   {
+     icon:'radio-button-on',
+     name:'Botones y router',
+     redirecTo:'/botones'
+   }
 
   ];
 
