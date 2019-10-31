4483d917e8b8197d01f2bef9c9665b7653b80ff3
fjuribe <fjuribeui@gmail.com>
2019-10-31 14:36:34 -0300

    Agregar componentes: ion-avatar| ion-chip

---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.page.html',
+  styleUrls: ['./alert.page.scss'],
+})
+export class AlertPage implements OnInit {
+ titulo:string;
+
+  constructor(public alertController: AlertController) { }
+
+
+  ngOnInit() {
+  }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+ async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Completa el campo',
+      inputs: [
+        {
+          name: 'nombre',
+          type: 'text',
+          placeholder: 'Nombre'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }, {
+          text: 'Enviar',
+          handler: (data) => {
+            console.log('Enviar',data);
+            this.titulo=data.nombre;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+}
