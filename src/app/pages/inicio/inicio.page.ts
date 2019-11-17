60a86ce4ea160ea17d2e4c2b88a4c6892ec89ab1
fjuribe <fjuribeui@gmail.com>
2019-11-17 00:34:03 -0300

    Agregando componente:ion-menu, con diferentes manera de implementar botones - Parte 1

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
@@ -94,9 +94,13 @@ export class InicioPage implements OnInit {
    }
   ];
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
+  }
+
+  toggleMenu(){
+    this.menu.toggle();
   }
 
 }
